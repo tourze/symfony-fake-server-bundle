4e25682b2932f8d5f0f26096f788282aaf9e61cd
github-ci <you@example.com>
2025-04-28 04:15:54 +0000

    feat(symfony-fake-server-bundle): 添加 Symfony Fake Server Bundle 功能

---
+++ src/EventSubscriber/RandomServerHeaderResponseSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tourze\FakeServerBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * 返回一个随机伪造的server，欺骗扫描器进行无效扫描
+ */
+class RandomServerHeaderResponseSubscriber
+{
+    private array $servers = [
+        'cloudflare',
+        'Windows-Azure-Web/1.0',
+        'Microsoft-HTTPAPI/2.0',
+        'Tengine',
+        'nginx',
+        'marco/3.2',
+        'JSP3/2.0.14',
+        'Microsoft-IIS/10.0',
+    ];
+
+    #[AsEventListener(event: KernelEvents::RESPONSE)]
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        if ($event->getResponse()->headers->has('Server')) {
+            return;
+        }
+
+        // 获取一个随机值
+        $server = $this->servers[array_rand($this->servers)];
+        $event->getResponse()->headers->set('Server', $server);
+    }
+}
---
+++ src/FakeServerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tourze\FakeServerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FakeServerBundle extends Bundle
+{
+}
